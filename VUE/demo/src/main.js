f957aa0a6535a9ce75fcff6f715e65e26be7c915
LiDeng <ChengduJackli@gmail.com>
2021-01-13 21:48:04 +0800

    vuex

--- VUE/demo/src/main.js
+++ VUE/demo/src/main.js
@@ -3,12 +3,26 @@ import router from './router';
 import App from './App.vue';
 import animate from 'animate.css';
  import './asset/base.scss';
+ import Vuex from 'vuex';
 
+
+
+ const store = new Vuex.Store({
+    state: {
+      count: 0
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      }
+    }
+  });
 
 Vue.use(animate);
-
+Vue.use(Vuex);
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 });
