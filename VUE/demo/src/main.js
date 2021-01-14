9e04e37738d1f42106ef0e0b44e462c64fcc686f
Jack D Li <Jack D Li@CNPF1GSR0J>
2021-01-14 18:57:15 +0800

    vuex

--- VUE/demo/src/main.js
+++ VUE/demo/src/main.js
@@ -3,10 +3,17 @@ import router from './router';
 import App from './App.vue';
 import animate from 'animate.css';
  import './asset/base.scss';
-
+ import Vuex from 'vuex';
+ import _ from 'lodash'
+ import { setToken } from './utils/js-cookie-lib'
  //token 
  setToken('Basic U0FQSk9CQVBJQUNDT1VOVDpTQVBKT0JBUElBQ0NPVU5U');
 
+ Object.defineProperty(Vue.prototype, '$_', { value: _ })
+  
+
+ Vue.use(animate);
+ Vue.use(Vuex);
 
  const store = new Vuex.Store({
     state: {
@@ -15,12 +22,14 @@ import animate from 'animate.css';
     mutations: {
       increment (state) {
         state.count++
-      }
+      },
+      decrement: state => state.count--,
+      
     }
   });
 
-Vue.use(animate);
-Vue.use(Vuex);
+
+  
 new Vue({
     el: '#app',
     router,
--- VUE/demo/src/views/subRouter3-vuex.vue
+++ VUE/demo/src/views/subRouter3-vuex.vue
@@ -1,23 +1,41 @@
 <template>
-    <div>
-  sub router 3 vuex study
-    </div>
+  <div>
+    sub router 3 vuex study
+    <button class="button" @click="increment">count ++</button>
+    <button class="button" @click="decrement">count --</button>
+    <div>count: {{ count }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "subRouter3 vuex",
-    //props: ['title'],
-    //每个 prop 都有指定的值类型
-    props: {
-        title: String,
-        age: Number,
-        isPublished: Boolean
+  name: "subRouter3 vuex",
+  //props: ['title'],
+  //每个 prop 都有指定的值类型
+  props: {
+    title: String,
+    age: Number,
+    isPublished: Boolean,
+  },
+  data() {
+    return {
+      msg: "base button",
+    };
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+      console.log("store count", this.$store.state.count);
     },
-    data() {
-        return {
-            msg: "base button"
-        };
-    }
+    decrement() {
+      this.$store.commit("decrement");
+      console.log("store count minus", this.$store.state.count);
+    },
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
 };
 </script>
